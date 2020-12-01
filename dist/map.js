f97bda65ffa9dc70d3230d0d2e335157c9c098e7
Brian Rutledge <brian@bhrutledge.com>
2020-11-30 21:29:56 -0500

    fixup! Download and join data in JS

--- dist/map.js
+++ dist/map.js
@@ -2,46 +2,43 @@
 Promise.all([
   fetch('https://docs.google.com/spreadsheets/d/1iY5wzVUpAKRHBF-vrwyUBz9_wV93FVqu4MW5viPSkas/export?gid=0&format=csv')
     .then((response) => response.text())
-    .then((csv) => {
-      const supporters = Papa.parse(csv, { header: true }).data;
-      const districtSupporters = supporters.reduce((acc, cur) => {
-        acc[cur.district] = cur;
-        return acc;
-      }, {});
-      return Promise.resolve(districtSupporters);
-    }),
+    .then((csv) => Promise.resolve(Papa.parse(csv, { header: true, dynamicTyping: true }).data)),
   fetch('https://raw.githubusercontent.com/bhrutledge/ma-legislature/main/dist/ma_house.geojson')
     .then((response) => response.json()),
 ])
-  .then(([districtSupporters, districtFeatures]) => {
-    const repsLayer = L.geoJson(districtFeatures, {
+  .then(([supporters, houseFeatures]) => {
+    const supportersByDistrict = supporters.reduce((acc, cur) => {
+      acc[cur.district] = cur;
+      return acc;
+    }, {});
+
+    const houseLayer = L.geoJson(houseFeatures, {
       onEachFeature(feature, layer) {
-        const rep = feature.properties;
-        const supporter = districtSupporters[rep.district] || {};
+        const rep = {
+          ...supportersByDistrict[feature.properties.district],
+          ...feature.properties,
+        };
 
         layer.bindPopup(`
-        <p><strong>${rep.district}</strong></p>
-        <p>
-          <img src="${rep.photo}" alt="Photo"><br>
-          <a href="${rep.url}">${rep.full_name}</a><br>
-          ${rep.party}<br>
-          <a href="mailto:${rep.email}">${rep.email}</a><br>
-          <a href="tel:${rep.phone}">${rep.phone}</a><br>
-        </p>
+          <p><strong>${rep.district}</strong></p>
+          <p>
+            <img src="${rep.photo}" alt="Photo"><br>
+            <a href="${rep.url}">${rep.full_name}</a><br>
+            ${rep.party}<br>
+            <a href="mailto:${rep.email}">${rep.email}</a><br>
+            <a href="tel:${rep.phone}">${rep.phone}</a><br>
+          </p>
 
-        <p>
-          Signed the pledge: ${supporter.pledge === 'yes' ? 'Yes' : 'Not yet'}</br>
-          Committed to vote: ${supporter.vote === 'yes' ? 'Yes' : 'Not yet'}</br>
-        </p>
-        <p>
-          <a href="https://actonmass.org/the-campaign/?your_state_representative=${rep.first_name} ${rep.last_name}">
-            <strong>Join the Campaign!</strong>
-          </a>
+          <p>
+            Signed the pledge: ${rep.pledge ? 'Yes' : 'Not yet'}</br>
+            Committed to vote: ${rep.vote ? 'Yes' : 'Not yet'}</br>
+          </p>
+          <p>
+            <a href="https://actonmass.org/the-campaign/?your_state_representative=${rep.first_name} ${rep.last_name}">
+              <strong>Join the Campaign!</strong>
+            </a>
+          </p>
         `);
-      },
-      style(feature) {
-        const rep = feature.properties;
-        const supporter = districtSupporters[rep.district] || {};
 
         let partyColor;
         switch (rep.party) {
@@ -55,17 +52,17 @@ Promise.all([
             partyColor = 'gray';
         }
 
-        return {
+        layer.setStyle({
           color: 'gray',
           weight: 1,
-          fillColor: supporter.vote === 'yes' ? 'green' : partyColor,
-          fillOpacity: supporter.pledge === 'yes' ? '0.6' : '0.3',
-        };
+          fillColor: rep.vote ? 'green' : partyColor,
+          fillOpacity: rep.pledge ? '0.6' : '0.3',
+        });
       },
     });
 
     L.map('map')
       .addLayer(L.tileLayer.provider('CartoDB.Positron'))
-      .addLayer(repsLayer)
-      .fitBounds(repsLayer.getBounds());
+      .addLayer(houseLayer)
+      .fitBounds(houseLayer.getBounds());
   });
