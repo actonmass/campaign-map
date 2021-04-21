6a43314bbd547b01c6658e1dd3ef7b0d1dbc6fcd
Brian Rutledge <brian@bhrutledge.com>
2021-04-21 06:28:53 -0400

    Update for People's House campaign

--- commitment-list.js
+++ commitment-list.js
@@ -1,6 +1,6 @@
 /* global Papa */
 document.addEventListener('DOMContentLoaded', () => {
-  const csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4l7bRcBIgwsEPGM_s9zF9csIeTgE2No_4tA6MuDCBUbfmWY_e9mAfzPpCJTsIK_hUzOyJ8CmdGMsX/pub?gid=641305740&single=true&output=csv';
+  const csv = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4l7bRcBIgwsEPGM_s9zF9csIeTgE2No_4tA6MuDCBUbfmWY_e9mAfzPpCJTsIK_hUzOyJ8CmdGMsX/pub?gid=2004051365&single=true&output=csv';
   Papa.parse(csv, {
     download: true,
     header: true,
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const commitments = [
           rep.committee_votes ? '1' : null,
           rep.public_bills ? '2' : null,
-          rep.roll_call ? '3' : null,
+          rep.term_limits ? '3' : null,
         ].filter((i) => i);
 
         repList.insertAdjacentHTML('beforeend', `
